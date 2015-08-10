d2783afd30879c67e798abde842df286af60b2c8
Joao Mendes <jjsm15@gmail.com>
2015-08-10 13:47:44 -0400

    Added a bit of CSS and maxed table size at 100

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
 angular.module('myApp', [])
     .controller('MultiplicationCtrl', function($scope) {
-        $scope.numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    
+    //Number of rows/columns on the table
+        $scope.tableLimit = 10;
+        $scope.numbers = function ()
+        {
+            var numArray = [];
+            //set a static limit of 100 rows and columns to keep user from trying large numbers
+            for (var i = 1; i <= 100; i++)
+            {
+             numArray.push(i); 
+            }
+            
+            if ($scope.tableLimit > 0)
+            { 
+                return numArray.slice(0, $scope.tableLimit);        
+            }
+            else
+            {
+                return defaultNo;
+            }
+        };
+        $scope.compute = function (a, b)
+        {
+         return a * b;   
+        };
     });
